32c3bb24022711e30d946815503624e617fb56a4
PavelKulkov <pawel.kulkoff@yandex.ru>
2015-12-23 16:09:29 +0300

    add Selector

---
+++ select/main.js
@@ -0,0 +1,10 @@
+function dark(){
+	$("*").css("color", "blue");
+	$("body").css("background", "black");
+	
+}
+function light(){
+	$("*").css("color", "red");
+	$("body").css("background", "#ffffcc");
+	
+}
